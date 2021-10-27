109496615c296fdc4e390709ca56de79a95b5751
Prajval H L <prajvalhl@gmail.com>
2021-10-27 19:36:36 +0530

    refactor: added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import { GetDadJoke } from "./GetDadJoke";
+import { Footer } from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Dad Jokes</h1>
       <GetDadJoke />
+      <Footer />
     </div>
   );
 }
